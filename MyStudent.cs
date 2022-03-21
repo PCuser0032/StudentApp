fb25b8971d54f80c129be69d6855f2b930ea11a2
user0032 <70772813+PCuser0032@users.noreply.github.com>
2022-03-21 18:18:29 +0300

    Add MyStudent.cs, create students. Implement add and getInfo methods.

---
+++ MyStudent.cs
@@ -0,0 +1,56 @@
+﻿namespace StudentActions
+{
+    internal class MyStudent
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Gender { get; set; }
+        public int Age { get; set; }
+        public string? HairColor { get; set; }
+        public string? EyeColor { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string? Group { get; set; }
+        public string? SpecialtyCode { get; set; }
+        public bool LiveOnDormitory { get; set; }
+
+        public static MyStudent Add(MyStudent x, MyStudent y)
+        {
+            MyStudent addResult = new()
+            {
+                Name = x.Name + y.Name,
+                Surname = x.Surname + y.Surname,
+                Gender = x.Gender + y.Gender,
+                Age = x.Age + y.Age,
+                HairColor = x.HairColor + y.HairColor,
+                EyeColor = x.EyeColor + y.EyeColor,
+                Height = x.Height + y.Height,
+                Weight = x.Weight + y.Weight,
+                Group = x.Group + y.Group,
+                SpecialtyCode = x.SpecialtyCode + y.SpecialtyCode,
+                LiveOnDormitory = x.LiveOnDormitory || y.LiveOnDormitory
+            };
+
+            return addResult;
+        }
+
+        public static void GetInfoAboutStudent(MyStudent x)
+        {
+            Console.WriteLine
+            (
+                $"Student info:\n" +
+                $"Name: { x.Name }\n" +
+                $"Surname: { x.Surname }\n" +
+                $"Gender: { x.Gender }\n" +
+                $"Age: { x.Age } years\n" +
+                $"Hair color: { x.HairColor }\n" +
+                $"Eye color: { x.EyeColor }\n" +
+                $"Height: { x.Height } m\n" +
+                $"Weight: { x.Weight } kg\n" +
+                $"Group: { x.Group }\n" +
+                $"Specialty code: { x.SpecialtyCode }\n" +
+                $"Live on dormitory: { (x.LiveOnDormitory ? "yes" : "no") }\n"
+            );
+        }
+    }
+}
---
+++ StudentApp.cs
@@ -0,0 +1,58 @@
+﻿using StudentActions;
+
+Console.WriteLine("StudentApp. Implementation actions with students on C#");
+Console.WriteLine("Created by Kozlov D., group VPI-31\n");
+
+MyStudent student1 = new MyStudent()
+{
+    Name = "Владимир",
+    Surname = "Копылов",
+    Gender = "м",
+    Age = 31,
+    HairColor = "Каштановый",
+    EyeColor = "Голубой",
+    Height = 1.85f,
+    Weight = 74.5f,
+    Group = "ВИС-41",
+    SpecialtyCode = "08.03.01",
+    LiveOnDormitory = true
+};
+
+MyStudent student2 = new()
+{
+    Name = "Ольга",
+    Surname = "Морозова",
+    Gender = "ж",
+    Age = 19,
+    HairColor = "Блонд",
+    EyeColor = "Карий",
+    Height = 1.61f,
+    Weight = 53.3f,
+    Group = "ВПР-11",
+    SpecialtyCode = "02.03.04",
+    LiveOnDormitory = false
+};
+
+MyStudent student3 = new()
+{
+    Name = "Валерия",
+    Surname = "Скрипка",
+    Gender = "м",
+    Age = 25,
+    HairColor = "Русый",
+    EyeColor = "Зелёный",
+    Height = 1.59f,
+    Weight = 48.9f,
+    Group = "ВИАС-33",
+    SpecialtyCode = "10.03.04",
+    LiveOnDormitory = true
+};
+
+// Get info about one student
+MyStudent.GetInfoAboutStudent(student1);
+
+// Get info about addition two students
+MyStudent.GetInfoAboutStudent(MyStudent.Add(student2, student3));
+
+// // Get info about addition three students
+MyStudent.GetInfoAboutStudent(MyStudent.Add(student1, MyStudent.Add(student2, student3)));
